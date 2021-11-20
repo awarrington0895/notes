a352f7fa029db33dd2224d02558f5a688aee46cc
Alex Warrington <awarrington0895@gmail.com>
2021-11-20 13:50:40 +0000

    Replace karma with jest

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,12 +1,11 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HomeComponent } from './home.component';
-import { OKTA_CONFIG, OktaAuthModule } from '@okta/okta-angular';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { OKTA_CONFIG, OktaAuthModule, OktaAuthStateService } from '@okta/okta-angular';
 import { RouterTestingModule } from '@angular/router/testing';
 import { OktaAuth } from '@okta/okta-auth-js';
+import { MockModule } from 'ng-mocks';
 
 describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
   const config = {
     issuer: 'https://not-real.okta.com',
     clientId: 'fake-client-id',
@@ -14,25 +13,21 @@ describe('HomeComponent', () => {
   };
   const oktaAuth = new OktaAuth(config);
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeComponent],
-      imports: [
-        RouterTestingModule,
-        OktaAuthModule
-      ],
-      providers: [{provide: OKTA_CONFIG, useValue: { oktaAuth }}]
-    })
-      .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  let spectator: Spectator<HomeComponent>;
+  const createComponent = createComponentFactory({
+    component: HomeComponent,
+    imports: [
+      RouterTestingModule,
+      MockModule(OktaAuthModule)
+    ],
+    providers: [{ provide: OKTA_CONFIG, useValue: { oktaAuth }}],
+    mocks: [OktaAuthStateService, OktaAuth]
   });
+  
+
+  beforeEach(() => spectator = createComponent());
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator).toBeTruthy();
   });
 });
